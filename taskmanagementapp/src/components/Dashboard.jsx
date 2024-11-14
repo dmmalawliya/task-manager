060ccd711f73dc479571c208be49150a4ccdf265
dmmalawliya <dmmalawliya@gmail.com>
2024-11-14 06:32:32 +0530

    updated dashboard

---
+++ taskmanagementapp/src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      {/* Additional sections for tasks can be added here */}
+    </div>
+  );
+}
+
+export default Dashboard;
