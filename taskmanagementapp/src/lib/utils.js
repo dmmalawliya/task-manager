1d238bcbbefeda30b82064003fcee252a28df2ef
dmmalawliya <dmmalawliya@gmail.com>
2024-11-14 20:17:16 +0530

    updated use-mobile hook

--- taskmanagementapp/src/lib/utils.js
+++ taskmanagementapp/src/lib/utils.js
@@ -1,6 +1,19 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import * as React from "react"
 
-export function cn(...inputs) {
-  return twMerge(clsx(inputs))
+const MOBILE_BREAKPOINT = 768
+
+export function useIsMobile() {
+  const [isMobile, setIsMobile] = React.useState(undefined)
+
+  React.useEffect(() => {
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+    const onChange = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    }
+    mql.addEventListener("change", onChange)
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    return () => mql.removeEventListener("change", onChange);
+  }, [])
+
+  return !!isMobile
 }
